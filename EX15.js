bcd79b0b7f54c94cabfd3de090b2cb8917e1d742
Myiah Moore <myiahsade@gmail.com>
2018-07-31 13:30:58 -0500

    check in

--- EX15.js
+++ EX15.js
@@ -4,4 +4,16 @@ function main()
 {
 var favNum = prompt("Enter your favorite number: ");
 alert(favNum);
+aboveOrBelow(favNum)
+}
+function aboveOrBelow(favNum)
+{
+    if (favNum < 100)
+    {
+        alert("Below")
+    }
+    else if (favNum > 100)
+    {
+        alert("Above");
+    }
 }
